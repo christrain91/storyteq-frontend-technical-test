a0acf055eeb3376ee7234016478476fc74b70901
Christopher Train <ctrain@pcs-publishing.com>
2024-06-25 14:26:32 +0100

    fix store initialisation

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import VehicleList from './components/VehicleList';
-import store from './state/store';
+import setupStore from './state/store';
 
 import './styles/global-styles.scss';
+
+const store = setupStore();
 
 ReactDOM.render(
   <React.StrictMode>
